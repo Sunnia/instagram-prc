38313bf3a643a24e46e1810db49a6198b391baf7
Michael Shih <Michael.Shih@abilitycorp.com.tw>
2018-10-19 18:39:22 +0800

    add save access token flow

--- InstagramApiPrc/InstagramApiPrc/InstagramAuthViewController.swift
+++ InstagramApiPrc/InstagramApiPrc/InstagramAuthViewController.swift
@@ -38,28 +38,24 @@ class InstagramAuthViewController: UIViewController {
     }
     
     func testSession() {
-        print("michael check \(#function) \(#line)")
         
         let authURL = String(format: "%@?client_id=%@&redirect_uri=%@&response_type=token&scope=%@", arguments: [API.INSTAGRAM_AUTHURL,API.INSTAGRAM_CLIENT_ID,API.INSTAGRAM_REDIRECT_URI,API.INSTAGRAM_SCOPE])
         let urlRequest = URLRequest.init(url: URL.init(string: authURL)!)
         webView.loadRequest(urlRequest)
         
-        print("michael check \(#function) \(#line)")
         
     }
     
     func checkRequestForCallbackURL(request: URLRequest) -> Bool {
-        print("michael check \(#function) \(#line)")
         let requestURLString = (request.url?.absoluteString)! as String
         if requestURLString.hasPrefix(API.INSTAGRAM_REDIRECT_URI) {
             let range: Range<String.Index> = requestURLString.range(of: "#access_token=")!
-            handleAuth(authToken: requestURLString.substring(from: range.upperBound))
+            handleAuth(authToken: String(requestURLString[range.upperBound...]))
             return false;
         }
         return true
     }
     func handleAuth(authToken: String) {
-        print("michael check \(#function) \(#line)")
         print("Instagram authentication token ==", authToken)
         self.delegate?.didGetAccessToken(access_token: authToken)
         self.dismiss(animated: true, completion: nil)
@@ -69,7 +65,6 @@ class InstagramAuthViewController: UIViewController {
 
 extension InstagramAuthViewController: UIWebViewDelegate{
     func webView(_ webView: UIWebView, shouldStartLoadWith request:URLRequest, navigationType: UIWebViewNavigationType) -> Bool{
-        print("michael check \(#function) \(#line)")
         return checkRequestForCallbackURL(request: request)
     }
 }
--- InstagramApiPrc/InstagramApiPrc/ViewController.swift
+++ InstagramApiPrc/InstagramApiPrc/ViewController.swift
@@ -9,15 +9,58 @@
 import UIKit
 
 
+class InstaInfoParameter {
+    var apUserDefaults :UserDefaults! = UserDefaults.standard
+    static private var Instance: InstaInfoParameter?
+    
+    var access_token: String = ""
+    
+    private init() {
+        access_token = defaultAccessToken()
+    }
+    
+    
+    func reset() {
+        access_token = defaultAccessToken()
+    }
+    
+    static func getInstance() -> InstaInfoParameter {
+        if let settingValue = Instance {
+            return settingValue
+        }
+        else {
+            Instance = InstaInfoParameter()
+            return Instance!
+        }
+    }
+    
+    private func defaultAccessToken()->String {
+        if let access_t = apUserDefaults.object(forKey: "access_token") as? String {
+            return access_t
+        } else {
+            return ""
+        }
+    }
+    
+    func save()  {
+        apUserDefaults.set(self.access_token, forKey: "access_token")
+    }
+}
 
 class ViewController: UIViewController {
     
-    var access_token = ""
+    var instaInfo: InstaInfoParameter = InstaInfoParameter.getInstance()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         //self.testSession()
+        if self.instaInfo.access_token != "" {
+            print("app start access_token is \(self.instaInfo.access_token)")
+        } else {
+            print("access_token unset")
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,9 +68,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //super.prepare(for: segue, sender: sender)
+        if segue.identifier == "goAuthWebView" {
+            let nextView = segue.destination as! InstagramAuthViewController
+            nextView.delegate = self
+        }
+    }
     
-    
-    @IBOutlet weak var webView: UIWebView!
     @IBAction func touchinsideGoooooalBtn() {
         
     }
@@ -37,6 +85,16 @@ class ViewController: UIViewController {
     
     
 
+}
+
+extension ViewController:InstaAuthDelegate{
+    func didGetAccessToken(access_token: String) {
+        self.instaInfo.access_token = access_token
+        self.instaInfo.save()
+        print("set token \(self.instaInfo.access_token)")
+    }
+    
+    
 }
 
 
