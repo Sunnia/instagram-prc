56daee1d4ee88851cadd2bb6a5adb24a7b3f9d22
Michael Shih <Michael.Shih@abilitycorp.com.tw>
2018-10-19 10:37:46 +0800

    start xcode project

---
+++ InstagramApiPrc/InstagramApiPrc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstagramApiPrc
+//
+//  Created by MichaelShih on 2018/10/19.
+//  Copyright © 2018 Sunnia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
